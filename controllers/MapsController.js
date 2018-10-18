a9d6027b5cfb38d8dc6ef86ef8a28fc9255089a3
Dice <diceroll123@gmail.com>
2018-10-18 02:54:52 -0400

    Removes unused (and erroneous) file system calls, also forces json object responses for strings

--- controllers/MapsController.js
+++ controllers/MapsController.js
@@ -1,58 +1,39 @@
-const fs = require("fs");
 const api = require('../api/battlefields.json');
 
 // Get all battlefields
 exports.getAllBattlefields = (req, res) => {
-  fs.readFile( api.toString(), 'utf8', function (err, data) {
     res.json( api.battlefields );
- });
 }
 
 // General Controllers
 exports.getBattlefieldByName = (req, res) => {
-  fs.readFile( api.toString(), 'utf8', function (err, data) {
     res.json( api.battlefields[req.params.battlefield] );
- });
 }
 
 exports.getBattlefieldName = (req, res) => {
-  fs.readFile( api.toString(), 'utf8', function (err, data) {
-    res.json( api.battlefields[req.params.battlefield].name );
- });
+    res.json( {"name": api.battlefields[req.params.battlefield].name } );
 }
 
 exports.getBattlefieldDescription = (req, res) => {
-  fs.readFile( api.toString(), 'utf8', function (err, data) {
-    res.json( api.battlefields[req.params.battlefield].description );
- });
+    res.json( {"description": api.battlefields[req.params.battlefield].description } );
 }
 
 exports.getBattlefieldCreep = (req, res) => {
-  fs.readFile( api.toString(), 'utf8', function (err, data) {
-    res.json( api.battlefields[req.params.battlefield].creep );
- });
+    res.json( {"creep": api.battlefields[req.params.battlefield].creep } );
 }
 
 exports.getBattlefieldHazard = (req, res) => {
-  fs.readFile( api.toString(), 'utf8', function (err, data) {
-    res.json( api.battlefields[req.params.battlefield].hazard );
- });
+    res.json( {"hazard": api.battlefields[req.params.battlefield].hazard } );
 }
 
 exports.getBattlefieldHazardText = (req, res) => {
-  fs.readFile( api.toString(), 'utf8', function (err, data) {
-    res.json( api.battlefields[req.params.battlefield].hazard_text );
- });
+    res.json( {"hazard_text": api.battlefields[req.params.battlefield].hazard_text } );
 }
 
 exports.getBattlefieldSplash = (req, res) => {
-  fs.readFile( api.toString(), 'utf8', function (err, data) {
-    res.json( api.battlefields[req.params.battlefield].splash_art );
- });
+    res.json( {"splash_art": api.battlefields[req.params.battlefield].splash_art } );
 }
 
 exports.getBattlefieldMap = (req, res) => {
-  fs.readFile( api.toString(), 'utf8', function (err, data) {
-    res.json( api.battlefields[req.params.battlefield].map );
- });
+    res.json( {"map": api.battlefields[req.params.battlefield].map } );
 }
